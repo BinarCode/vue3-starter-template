c82be410d50ef166497be2fd08df22309f02b444
cristij <joracristi@gmail.com>
2021-11-10 13:05:10 +0200

    Add postcss and tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enable: true,
+    content: [
+      './index.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}',
+      './safelist.txt'
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
