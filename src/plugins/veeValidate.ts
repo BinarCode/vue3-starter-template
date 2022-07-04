4903891629aaea1c1e855fe21ad6ad053f951399
cristij <joracristi@gmail.com>
2022-07-04 15:20:47 +0300

    Remove already declared vee validate rules

--- src/plugins/veeValidate.ts
+++ src/plugins/veeValidate.ts
@@ -15,19 +15,5 @@ export default {
       if (typeof validationRule === 'function')
         defineRule(rule, validationRule)
     })
-
-    defineRule('email', (value: string) => {
-      if (!value || /(^$)|^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value))
-        return true
-
-      return 'This field must be a valid email'
-    })
-
-    defineRule('url', (value: string) => {
-      if (!/(^$)|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}/.test(value))
-        return 'This field must be a valid url'
-
-      return true
-    })
   },
 }
